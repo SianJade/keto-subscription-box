89d5c37ff19620f8bb01ced8e0fe916a263aed2f
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-13 09:46:34 +0000

    Add logout function

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import auth
 
 def index(request):
     """
     Return index.html page
     """
     return render(request, 'index.html')
+
+
+def logout(request):
+    """
+    Log user out
+    """
+    auth.logout(request)
+    return redirect(reverse('index'))
