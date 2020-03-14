6a945b3e6a885f4ee1b0fbd6e44ec151d05c13a4
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-14 12:34:29 +0000

    Create function to display login page

--- accounts/views.py
+++ accounts/views.py
@@ -15,3 +15,10 @@ def logout(request):
     auth.logout(request)
     messages.success(request, "Logout successful")
     return redirect(reverse('index'))
+
+
+def login(request):
+    """
+    Return login page
+    """
+    return render(request, 'login.html')
