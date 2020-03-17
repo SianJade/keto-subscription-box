b169c739fa4b890b03612753fa501fc059051d97
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-17 12:09:35 +0000

    Add account related url patterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,10 @@
 from django.conf.urls import url
 from accounts.views import index, logout, login, registration, user_profile
 import urls_reset
+
+urlpatterns = [
+    url(r'^logout/$', logout, name="logout"),
+    url(r'^login/$', login, name="login"),
+    url(r'^register/$', registration, name="registration"),
+    url(r'^profile/$', user_profile, name="profile")
+]
