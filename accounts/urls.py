d38e18083682085339e90bcabbef778cd6c829ba
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-17 12:12:20 +0000

    Add password reset url pattern

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from accounts.views import index, logout, login, registration, user_profile
 import urls_reset
 
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^logout/$', logout, name="logout"),
     url(r'^login/$', login, name="login"),
     url(r'^register/$', registration, name="registration"),
-    url(r'^profile/$', user_profile, name="profile")
+    url(r'^profile/$', user_profile, name="profile"),
+    url(r'^password_reset/$', include(urls_reset))
 ]
