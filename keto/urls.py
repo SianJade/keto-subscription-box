d0b1fdc7fa05213c1dd2537ea72dd13d1f899f9c
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-24 10:28:47 +0000

    Import all products views and create url pattern

--- keto/urls.py
+++ keto/urls.py
@@ -17,9 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from home.views import index
 from accounts import urls as accounts_urls
+from all_products import urls as products_urls
+from all_products.views import all_products
+from django.views import static
+from .settings import MEDIA_ROOT
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
-    url(r'^accounts/', include(accounts_urls))
+    url(r'^accounts/', include(accounts_urls)),
+    url(r'^all_products/', all_products, name="allProducts")
 ]
