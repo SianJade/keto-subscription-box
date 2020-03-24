2a1f9d6d4c21bf21598953861ecaeecdf6b6a27e
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-24 15:27:00 +0000

    Remove unnecessary argument from all products url

--- keto/urls.py
+++ keto/urls.py
@@ -26,6 +26,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
-    url(r'^all_products/', include(products_urls), all_products),
+    url(r'^all_products/', include(products_urls)),
     url(r'^media/(?P<path>.*$)', static.serve, {'document_root': MEDIA_ROOT}),
 ]
