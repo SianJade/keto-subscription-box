0f996a9fd2b4ef0b0caf19f6ecc034ac283f91a4
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-24 10:31:51 +0000

    Add media root url

--- keto/urls.py
+++ keto/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
-    url(r'^all_products/', all_products, name="allProducts")
+    url(r'^all_products/', include(products_urls), all_products),
+    url(r'^media/(?P<path>.*$)', static.serve, {'document_root': MEDIA_ROOT}),
 ]
