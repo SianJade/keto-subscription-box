97a892aaf67f73d39dfc84af0419ed361127f1e3
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-25 13:43:13 +0000

    Add url to add items to the shopping cart

--- all_products/urls.py
+++ all_products/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url, include
 from .views import all_products
+from shopping_cart.views import add_to_cart
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
+    url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
 ]
