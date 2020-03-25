0dce37c9666e948fb738e51fe9a09fa8a86e6fbf
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-25 17:44:42 +0000

    Add cart and search url patterns

--- keto/urls.py
+++ keto/urls.py
@@ -29,5 +29,7 @@ urlpatterns = [
     url(r'^$', index, name="index"),
     url(r'^accounts/', include(accounts_urls)),
     url(r'^all_products/', include(products_urls)),
+    url(r'^cart/', include(shopping_cart_urls)),
+    url(r'^search/', include(search_urls)),
     url(r'^media/(?P<path>.*$)', static.serve, {'document_root': MEDIA_ROOT}),
 ]
