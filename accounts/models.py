1c74dffb77b4b64ce3d66f299d612b199bc0ae45
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-29 08:45:41 +0000

    Create customer model to extend django's defaul user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=False)
+    telephone = models.IntegerField(default='', blank=False)
+    address_1 = models.CharField(max_length=75, default='', blank=False)
+    address_2 = models.CharField(max_length=75, default='', blank=True)
+    city = models.CharField(max_length=75, default='', blank=False)
+    county = models.CharField(max_length=75, default='', blank=True)
+    postcode = models.CharField(max_length=8, default='', blank=False)
+    dietary_choices = (
+    ('NONE', 'None'),
+    ('VEGETARIAN', 'Vegetarian'),
+    ('VEGAN', 'Vegan'),
+    ('PESCETARIAN', 'Pescetarian'),
+    ('KOSHER', 'Kosher'),
+    ('HALAL', 'Halal'),
+    )
+    dietary_preference = models.CharField(max_length=50, choices=dietary_choices, null=True, blank=False)
+    allegeries_intolerances_choices = (
+    ('NONE', 'None'),
+    ('NUTS', 'Nuts'),
+    ('Milk', 'Milk'),
+    ('LACTOSE', 'Lactose'),
+    ('EGG', 'Egg'),
+    ('FISH', 'Fish'),
+    ('SHELLFISH', 'Shellfish'),
+    ('GLUTEN', 'Gluten'),
+    ('SOY', 'Soy'),
+    ('WHEAT', 'Wheat'),
+    )
+    allegeries_intolerances = models.CharField(max_length=50, choices=allegeries_intolerances_choices, null=True, blank=False)
