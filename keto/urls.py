16de8363739b9a397c9d12622dfe135c407a68a8
Ubuntu <ubuntu@ip-172-31-33-205.ec2.internal>
2020-03-31 08:09:09 +0000

    Import subscription urls to main urls file

--- keto/urls.py
+++ keto/urls.py
@@ -22,6 +22,7 @@ from all_products.views import all_products
 from shopping_cart import urls as shopping_cart_urls
 from search import urls as search_urls
 from checkout import urls as checkout_urls
+from subscribe import urls as subscription_urls
 from django.views import static
 from .settings import MEDIA_ROOT
 
@@ -33,5 +34,6 @@ urlpatterns = [
     url(r'^cart/', include(shopping_cart_urls)),
     url(r'^search/', include(search_urls)),
     url(r'^checkout/', include(checkout_urls)),
+    url(r'^subscribe/', include(subscription_urls)),
     url(r'^media/(?P<path>.*$)', static.serve, {'document_root': MEDIA_ROOT}),
 ]
