fe3afa58b8692b30b66a2dfd8290d4701dac09af
Sian <sian.jade.llewellyn@gmail.com>
2020-04-01 15:16:05 +0000

    Add category argument to add to cart function

--- shopping_cart/views.py
+++ shopping_cart/views.py
@@ -7,26 +7,27 @@ def view_cart(request):
     return render(request, 'cart.html')
 
 
-def add_to_cart(request, id):
+def add_to_cart(request, id, category):
     """
     Add selected item to cart
     """
     quantity=int(request.POST.get('quantity'))
     
-    cart = request.session.get('cart', {})
-    if id in cart:
+    cart = request.session.get('cart', { 'product': {}, 
+                                         'subscription': {} } )
+    if id in cart[category]:
         """
         If the id of the selected product is already in the user's shopping cart
         and they wish to add another of the same item, then get the integer that
         is the product quantity and add the new product quantity to it
         """
-        cart[id] = int(cart[id]) + quantity
+        cart[category][id] = int(cart[category][id]) + quantity
     else:
         """
         If the item with the selected id is not already in the user's cart, then
         add the specified quantity of the selected item to the user's cart
         """
-        cart[id] = cart.get(id, quantity)
+        cart[category][id] = cart[category].get(id, quantity)
     
     request.session['cart'] = cart
     return redirect(reverse('index'))
