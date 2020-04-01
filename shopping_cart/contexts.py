40eeb4b5d6258434fc26d04099a470db48ea6d74
Sian <sian.jade.llewellyn@gmail.com>
2020-04-01 06:55:03 +0000

    Modify cart contents function to include subscription purchases as well as standalone product purchases

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from all_products.models import Product
+from subscribe.models import Subscription
 
 def cart_contents(request):
     """
@@ -14,12 +15,13 @@ def cart_contents(request):
     product_count = 0
     for id, quantity in cart.items():
         product = get_object_or_404(Product, pk=id)
+        subscription = get_object_or_404(Subscription, pk=id)
         """
         Total is a running cost of product price multiplied by product quantity
         """
         total += quantity * product.price
         product_count += quantity
-        cart_items.append({'id': id, 'quantity': quantity, 'product': product})
+        cart_items.append({'id': id, 'quantity': quantity, 'product': product, 'subscription': subscription})
     """
     Return a dictionary of key value pairs for cart items, total, and product count
     """
