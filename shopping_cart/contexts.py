49c0e85c0b7f8053490fe1a7e7cb6412ffc3d7ce
Sian <sian.jade.llewellyn@gmail.com>
2020-04-02 08:45:54 +0000

    Add .items() to loop through product and subscription dictionaries, remove print statements

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -7,26 +7,21 @@ def cart_contents(request):
     Ensures cart contents are available when rendering every page across the site
     """
     cart = request.session.get('cart', { 'product': {}, 'subscription': {}})
-    print(cart)
-    # cart = { 'product': {1 : 1}, 'subscription': {2 : 1}}
     cart_items = []
-    print(bool(cart['product']))
-    print(bool(cart['subscription']))
     """
     Initialize cart total and product count
     """
     total = 0
     product_count = 0
     if bool(cart['product']):
-        print(type(cart['product']))
-        for product_id, quantity in cart['product']:
+        for product_id, quantity in cart['product'].items():
             product = get_object_or_404(Product, pk=product_id)
             total += product.price * quantity
             product_count += quantity
             cart_items.append({'product_id': product_id, 'quantity': quantity})
 
     if bool(cart['subscription']):
-        for subscription_id, quantity in cart['subscription']:
+        for subscription_id, quantity in cart['subscription'].items():
             subscription = get_object_or_404(Subscription, pk=subscription_id)
             total += subscription.price * quantity
             product_count += quantity
