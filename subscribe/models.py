1f8adf8b6f34d6bf96b76f23e148995bf98d1955
Sian <sian.jade.llewellyn@gmail.com>
2020-04-02 09:39:14 +0000

    Change 'duration' to 'name'

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Subscription(models.Model):
-    duration = models.CharField(max_length=50, default='')
+    name = models.CharField(max_length=50, default='')
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
-        return self.duration
+        return self.name
