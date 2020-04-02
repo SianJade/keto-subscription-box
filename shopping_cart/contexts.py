11d1bbf78e6c4d0590c16cf00f13b5a0d2c179d4
Sian <sian.jade.llewellyn@gmail.com>
2020-04-02 08:12:51 +0000

    Add print statements to check if product in cart is true and what the type of the product in the cart is

--- shopping_cart/contexts.py
+++ shopping_cart/contexts.py
@@ -7,23 +7,30 @@ def cart_contents(request):
     Ensures cart contents are available when rendering every page across the site
     """
     cart = request.session.get('cart', { 'product': {}, 'subscription': {}})
+    print(cart)
+    # cart = { 'product': {1 : 1}, 'subscription': {2 : 1}}
     cart_items = []
+    print(bool(cart['product']))
+    print(bool(cart['subscription']))
     """
     Initialize cart total and product count
     """
     total = 0
     product_count = 0
-    for product_id, quantity in cart['product']:
-        product = get_object_or_404(Product, pk=product_id)
-        total += product.price * quantity
-        product_count += quantity
-        cart_items.append({'product_id': product_id, 'quantity': quantity})
+    if bool(cart['product']):
+        print(type(cart['product']))
+        for product_id, quantity in cart['product']:
+            product = get_object_or_404(Product, pk=product_id)
+            total += product.price * quantity
+            product_count += quantity
+            cart_items.append({'product_id': product_id, 'quantity': quantity})
 
-    for subscription_id, quantity in cart['subscription']:
-        subscription = get_object_or_404(Subscription, pk=subscription_id)
-        total += subscription.price * quantity
-        product_count += quantity
-        cart_items.append({'subscription_id': subscription_id, 'quantity': quantity})
+    if bool(cart['subscription']):
+        for subscription_id, quantity in cart['subscription']:
+            subscription = get_object_or_404(Subscription, pk=subscription_id)
+            total += subscription.price * quantity
+            product_count += quantity
+            cart_items.append({'subscription_id': subscription_id, 'quantity': quantity})
 
     """
     Return a dictionary of key value pairs for cart items, total, and product count
