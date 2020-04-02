044b61808b9bc8a9d5e285a5b093b7ce290e3713
Sian <sian.jade.llewellyn@gmail.com>
2020-04-02 10:11:10 +0000

    Add subcription order line item

--- checkout/models.py
+++ checkout/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from all_products.models import Product
+from subscribe.models import Subscription
 
 
 class Order(models.Model):
@@ -24,3 +25,11 @@ class OrderLineItem(models.Model):
 
     def __str__(self):
         return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
+
+class SubscriptionOrderLineItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, null=False)
+    quantity = models.IntegerField(blank=False)
+
+    def __str__(self):
+        return "{0} {1} @ {2}".format(self.quantity, self.subscription.name, self.subscription.price)
