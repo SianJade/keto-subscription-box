eb671cf0da8182d1ca0275678fe48006e97da49e
Sian <sian.jade.llewellyn@gmail.com>
2020-04-05 09:41:49 +0000

    Add view product url

--- all_products/urls.py
+++ all_products/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url, include
-from .views import all_products
+from .views import all_products, view_product
 from shopping_cart.views import add_to_cart
 
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
+    url(r'^product/(?P<id>\d+)', view_product, name='view_product')
 ]
