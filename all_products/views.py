8da43af228c93ccc3254706cf42abf62de1ffd90
Sian <sian.jade.llewellyn@gmail.com>
2020-04-05 09:41:20 +0000

    Add view for viewing individual product info

--- all_products/views.py
+++ all_products/views.py
@@ -4,3 +4,8 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
     return render(request, "products.html", {"products":  products})
+
+def view_product(request, id):
+    products = Product.objects.all()
+    product = Product, id
+    return render(request, "product.html", {"product": product})
