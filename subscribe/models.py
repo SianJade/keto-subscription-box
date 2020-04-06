8e4afaa2c224f050cd4c4c9d8413b03828d058b8
Sian <sian.jade.llewellyn@gmail.com>
2020-04-06 09:19:11 +0000

    Remove default value from subscription name field

--- subscribe/models.py
+++ subscribe/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from accounts.models import Customer
+
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=50, default='')
+    name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     image = models.ImageField(upload_to='images')
     
     def __str__(self):
         return self.name
+
+
+class CustomerSubscription(models.Model):
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
