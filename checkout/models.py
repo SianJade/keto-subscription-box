20ca59231293fa95a2bc9b535cc7e1e66c2d2b53
Sian <sian.jade.llewellyn@gmail.com>
2020-04-06 09:46:31 +0000

    Add foreign key to customer model in order model

--- checkout/models.py
+++ checkout/models.py
@@ -1,34 +1,29 @@
 from django.db import models
 from all_products.models import Product
 from subscribe.models import Subscription
+from accounts.models import Customer
 
 
 class Order(models.Model):
-    full_name = models.CharField(max_length=50, blank=False)
-    phone_number = models.CharField(max_length=20, blank=False)
-    country = models.CharField(max_length=40, blank=False)
-    postcode = models.CharField(max_length=20, blank=True)
-    town_or_city = models.CharField(max_length=40, blank=False)
-    street_address1 = models.CharField(max_length=40, blank=False)
-    street_address2 = models.CharField(max_length=40, blank=False)
-    county = models.CharField(max_length=40, blank=False)
-    date = models.DateField()
+    customer = models.ForeignKey(Customer)
+    total = models.DecimalField(max_digits=10, decimal_places=2)
+    order_timestamp = models.DateTimeField()
 
     def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+        return "{0}-{1}-{2}".format(self.id, self.total, self.customer.name)
 
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
         return "{0} {1} @ {2}".format(self.quantity, self.product.name, self.product.price)
 
 class SubscriptionOrderLineItem(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=False)
-    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, null=False)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    subscription = models.ForeignKey(Subscription, on_delete=models.CASCADE, null=True)
     quantity = models.IntegerField(blank=False)
 
     def __str__(self):
