f4aba373d6107687656a673bb09a8c0bb1a2d7ca
Sian <sian.jade.llewellyn@gmail.com>
2020-04-07 07:27:29 +0000

    Re-enter full order model without foriegn keys to other models

--- checkout/models.py
+++ checkout/models.py
@@ -5,12 +5,19 @@ from accounts.models import Customer
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer)
-    total = models.DecimalField(max_digits=10, decimal_places=2)
-    order_timestamp = models.DateTimeField()
+    full_name = models.CharField(max_length=50, blank=False, default='')
+    phone_number = models.CharField(max_length=20, blank=False, default='')
+    country = models.CharField(max_length=40, blank=False, default='')
+    postcode = models.CharField(max_length=20, blank=True, default='')
+    town_or_city = models.CharField(max_length=40, blank=False, default='')
+    street_address1 = models.CharField(max_length=40, blank=False, default='')
+    street_address2 = models.CharField(max_length=40, blank=False, default='')
+    county = models.CharField(max_length=40, blank=False, default='')
+    date = models.DateField()
 
     def __str__(self):
-        return "{0}-{1}-{2}".format(self.id, self.total, self.customer.name)
+        return "{0}-{1}-{2}".format(self.id, self.date, self.full_name)
+
 
 
 class OrderLineItem(models.Model):
