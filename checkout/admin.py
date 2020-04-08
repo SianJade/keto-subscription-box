e1519e6d7194d9d6756cc5512cb5418d49b3f6a4
Sian <sian.jade.llewellyn@gmail.com>
2020-04-08 09:13:58 +0000

    Register SubscriptionOrderLineItem in admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Order, OrderLineItem
+from .models import Order, OrderLineItem, SubscriptionOrderLineItem
 
 
 class OrderLineAdminInline(admin.TabularInline):
     model = OrderLineItem
 
+class SubscriptionAdminInline(admin.TabularInline):
+    model = SubscriptionOrderLineItem
+
 class OrderAdmin(admin.ModelAdmin):
-    inlines = (OrderLineAdminInline, )
+    inlines = (OrderLineAdminInline, SubscriptionAdminInline)
 
 admin.site.register(Order, OrderAdmin)
