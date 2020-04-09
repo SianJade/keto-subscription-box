7ef1cbda8edb6448ee5b418b7a990c34b4bcf1a0
Sian <sian.jade.llewellyn@gmail.com>
2020-04-09 09:31:09 +0000

    Add get object or 404 to view for individual product

--- all_products/views.py
+++ all_products/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
+    """
+    Shows the user all products available for purchase on one page
+    """
     products = Product.objects.all()
     return render(request, "products.html", {"products":  products})
 
-def view_product(request, id):
-    products = Product.objects.all()
-    product = Product, id
+
+def view_product(request, pk):
+    """
+    View more detailed information about a single product on its own page
+    """
+    product = get_object_or_404(Product, pk=id)
     return render(request, "product.html", {"product": product})
