da34a84e489cea35079369cce6c35425e8c05735
Sian <sian.jade.llewellyn@gmail.com>
2020-04-09 09:32:04 +0000

    Change view product url to contain id rather than pk

--- all_products/urls.py
+++ all_products/urls.py
@@ -5,5 +5,5 @@ from shopping_cart.views import add_to_cart
 urlpatterns = [
     url(r'^$', all_products, name='products'),
     url(r'^add/(?P<id>\d+)', add_to_cart, name='add_to_cart'),
-    url(r'^product/(?P<id>\d+)', view_product, name='view_product')
+    url(r'^product/(?P<pk>\d+)/$', view_product, name='view_product')
 ]
