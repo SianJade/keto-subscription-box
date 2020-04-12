200cfc53f6f77080bada1c8eec5f7ea774024776
Sian <sian.jade.llewellyn@gmail.com>
2020-04-12 08:14:44 +0000

    Add product ingredients variable to product view

--- all_products/views.py
+++ all_products/views.py
@@ -14,4 +14,5 @@ def view_product(request, product_id):
     View more detailed information about a single product on its own page
     """
     product = get_object_or_404(Product, id=product_id)
-    return render(request, "product.html", {"product": product})
+    ingredients = get_object_or_404(ProductIngredients, id=product_id)
+    return render(request, "product.html", {"product": product, "ingredients": ingredients})
