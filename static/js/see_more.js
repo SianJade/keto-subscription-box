eea8f696205688f05f7354b9a8325be896d7a0dc
Sian <sian.jade.llewellyn@gmail.com>
2020-04-13 11:43:53 +0000

    Add see more button functionality

--- static/js/see_more.js
+++ static/js/see_more.js
@@ -1,1 +1,16 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+    function seeMore(element) {
+
+    let buttonClicked = event.target;
+    let more = document.getElementById(element);
+    if (more.style.display === "none") {
+        buttonClicked.innerText = "See Less";
+        more.style.display = "block";
+    }
+    else {
+        buttonClicked.innerText = "See More";
+        more.style.display = "none";
+    }
+}
+seeMore();
+});
