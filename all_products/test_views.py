e58c25fc360add3e319a8d3fb17790edd43e1e76
Sian <sian.jade.llewellyn@gmail.com>
2020-04-14 12:47:10 +0000

    Add test for product views

---
+++ all_products/test_views.py
@@ -0,0 +1,7 @@
+from django.test import TestCase
+from .models import Product
+
+class TestProductViews(TestCase):
+    def test_products_page_can_be_viewed(self):
+        page=self.client.get('/all_products/')
+        self.assertEqual(page.status_code, 200)
