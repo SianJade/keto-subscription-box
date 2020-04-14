acec84ca633d647967d53e2cecfc07854af3b729
Sian <sian.jade.llewellyn@gmail.com>
2020-04-14 10:01:51 +0000

    Add tests for Ingredients model

--- all_products/test_models.py
+++ all_products/test_models.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from .models import Product, NutritionValue
+from .models import Product, NutritionValue, Ingredients, ProductIngredients
 
 
 class TestProductCreation(TestCase):
@@ -32,3 +32,12 @@ class TestNutritionValues(TestCase):
         self.assertFalse(nutrition.fibre)
         self.assertFalse(nutrition.protein)
         self.assertEqual(nutrition.salt, '0.01')
+
+
+class TestIngredientModel(TestCase):
+    def test_ingredients(self):
+        ingredient = Ingredients(name='baking powder')
+        self.assertEqual(ingredient.name, 'baking powder')
+        self.assertNotEqual(ingredient.name, 'flour')
+
+
