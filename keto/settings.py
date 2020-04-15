3c63aab57eb6e7482a6b6095ab8453e70ec3fc42
Sian <sian.jade.llewellyn@gmail.com>
2020-04-15 10:26:20 +0000

    Add email backend to settings.py

--- keto/settings.py
+++ keto/settings.py
@@ -172,8 +172,5 @@ STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
-EMAIL_PORT = 587
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SYSTEM_EMAIL = 'keto-box@example.com'
