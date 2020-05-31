f855c32de5f0d1a0cfadb44c0cbe9392b77b4809
Sian <sian.jade.llewellyn@gmail.com>
2020-05-31 10:00:32 +0000

    Remove unneeded comments from py files in all_products app

--- all_products/test_models.py
+++ all_products/test_models.py
@@ -4,10 +4,6 @@ from .models import Product, NutritionValue, Ingredients, ProductIngredients
 
 class TestProductCreation(TestCase):
     def test_product_creation(self):
-        """
-        Assertions are listed in the order in which
-        the fields appear in the Product model
-        """
         product = Product(name='New Product',
                           description='A tasty beverage',
                           price='1.99')
@@ -21,10 +17,6 @@ class TestProductCreation(TestCase):
 
 class TestNutritionValues(TestCase):
     def test_nutrition_values(self):
-        """
-        Assertions are listed in the order in which
-        the fields appear in the NutritionValue model
-        """
         nutrition = NutritionValue(calories_per_serving='123',
                                    carbs='2.4',
                                    sugars='2',
--- all_products/views.py
+++ all_products/views.py
@@ -3,17 +3,11 @@ from .models import Product, Ingredients, ProductIngredients, NutritionValue
 
 
 def all_products(request):
-    """
-    Shows the user all products available for purchase on one page
-    """
     products = Product.objects.all()
     return render(request, "products.html", {"products":  products})
 
 
 def view_product(request, id):
-    """
-    View more detailed information about a single product on its own page
-    """
     product = get_object_or_404(Product, id=id)
     nutrition = get_object_or_404(NutritionValue, id=id)
     ingredients = product.ingredients.all()
